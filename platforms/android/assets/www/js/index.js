87058c17796501e6a1d570498a314ad9dca325eb
Baskoff <Baskoff@192.168.56.1>
2016-12-07 17:24:35 +0700

    07.12.2016 17:24

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,21 +1,47 @@
 jQuery(document).ready(function () {
-    jQuery(".phoneMask").mask("+7 (999) 999-9999");    
     jQuery.ajax({
         type: "POST",
         url: "http://tele1000.ru/api/1/Phones_Group/GetGroups/",
-        //data: ({a : aaa, b : bbb}),
+        dataType: 'json',
         success: function (data) {
-            try {
-                var pdata = JSON.parse(data);
-            } catch (err) {
-                alert("Ошибка, Попробуйте позже.");
-                alert(data);
-            }
             //jQuery("textarea").val(location.href);
-            for (var key in pdata.records) {
-                var item = pdata.records[key];
+            for (var key in data.records) {
+                var item = data.records[key];
                 jQuery("select[name=group]").append('<option value="'+item.phones_group_id+'">'+item.phones_group_name+'</option>');
             }
         }
-    });  
+    }); 
+    var now = new Date();
+    jQuery("input[name=date]").val(now.toString('yyyy-MM-dd'));
+    jQuery("input[name=time]").val(now.toString('HH:mm'));
+    
+    jQuery.ajax({
+        type: "POST",
+        url: "http://tele1000.ru/api/1/Cabinet/GetHistory/",
+        dataType: 'json',
+        success: function (data) {
+            //tableHistory.records = data;
+             for (var i = 0; i < data.length; i++) {
+                 var item = data[i];
+                 var html = '<tr>\n\
+                                <td>'+item.phones_calls_id+'</td>\n\
+                                <td><a href="phones.html?id='+item.phones_calls_owner[0].phones_id+'">'+item.phones_calls_owner[0].phones_name+'</a></td>\n\
+                                <td>'+item.phones_calls_dt+'</td>\n\
+                                <td>'+item.phones_calls_group[0].phones_group_name+'</td>\n\
+                                <td>'+item.phones_calls_owner[0].phones_user[0].user_name+'</td>\n\
+                                <td>'+item.phones_calls_clarification+'</td>\n\
+                                <td>'+item.phones_calls_owner[0].phones_body+'</td>\n\
+                            </tr>';
+                jQuery("#historyTable tbody").append(html);
+             }
+        }
+    });
+    
+    
+    var test = new Vue({
+        el: "#test",
+        data: {
+            text: "ntcn",
+        }
+    });
 });
--- platforms/browser/www/js/main.js
+++ platforms/browser/www/js/main.js
@@ -1,4 +1,29 @@
+var user = null;
 jQuery(document).ready(function () {
+    //Проверим юзера и если нету - на авторизацию.
+    var path = window.location.pathname;
+    if (path != "/auth.html" && path != "/reg.html") {
+        jQuery.ajax({
+            type: "POST",
+            url: "http://tele1000.ru/api/1/Auth/GetUser/",
+            //data: ({a : aaa, b : bbb}),
+            success: function (data) {
+                try {
+                    var pdata = JSON.parse(data);
+                } catch (err) {
+                    alert("Ошибка, Попробуйте позже.");
+                    alert(data);
+                }
+                if (pdata != null) {
+                    user = pdata;
+                } else {
+                     document.location.href='auth.html'
+                }
+            }
+        });         
+    };
+    jQuery(".phoneMask").mask("+7 (999) 999-9999");    
+    
     jQuery("body").on("click", "a", function () {
         var text = jQuery(this).attr("question");
         if (text != undefined)
