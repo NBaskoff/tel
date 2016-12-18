6938ae9aa4086bde939fa81d01da8d7dafdb58e5
Baskoff <Baskoff@192.168.56.1>
2016-12-18 18:24:30 +0700

    18.12.2016 18:24

--- platforms/browser/www/js/phones.js
+++ platforms/browser/www/js/phones.js
@@ -9,6 +9,9 @@ jQuery(document).ready(function () {
             jQuery(".phoneName").html(data.record.phones_name);
             jQuery(".phoneGroup").html(data.record.phones_group[0].phones_group_name);
             jQuery(".phoneUser").html(data.record.phones_user[0].user_name);
+            if (data.record.phones_user[0].user_name != undefined) {
+                jQuery(".addContact").css("display", "block");
+            }
             jQuery(".phoneBody").html(data.record.phones_body);
             jQuery(".phoneLink").attr("href", "cabinet.html?phone="+encodeURIComponent(data.record.phones_name));
             for (var i = 0; i < data.record.phones_files.length; i++) {
@@ -32,4 +35,30 @@ jQuery(document).ready(function () {
              }
         }
     });
+    jQuery(".addContact").click(function() {
+        var user = jQuery(".phoneUser").html();
+        var myContact = navigator.contacts.create({"displayName": user});
+        var name = new ContactName();
+        name.givenName = user;
+        myContact.name = name;
+
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('work', jQuery(".phoneName").html(), false);
+        //phoneNumbers[1] = new ContactField('mobile', '917-555-5432', true); // preferred number
+        //phoneNumbers[2] = new ContactField('home', '203-555-7890', false);
+        myContact.phoneNumbers = phoneNumbers;
+
+        myContact.note = "";
+
+        myContact.save(onSuccessCallBack, onErrorCallBack);
+        
+        function onSuccessCallBack(contact) {
+            alert("Контакт сохранён");
+        };
+
+        function onErrorCallBack(contactError) {
+            alert("Error = " + contactError.code);
+        };        
+    });
+    
 });
